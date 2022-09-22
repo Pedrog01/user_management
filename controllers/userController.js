d22e8be7809ca247222c28d7a40d09ad20b15540
Pedro Gabriel <pedro7109.j@gmail.com>
2022-09-22 11:11:13 -0300

    Working with Editing Data - Scheduling the Event on the Edit Button.

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,9 +6,21 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit()
+        this.onEdit();
 
     }
 
+        onEdit(){
+
+            document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+    
+                this.showPanelCreate();
+    
+            });
+    
+        }
+
+        
     onSubmit(){
 
         this.formEl.addEventListener("submit", event => {
@@ -137,21 +149,44 @@ class UserController {
         tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
-    
-            <td><img src=${dataUser.photo} alt="User Image" class="img-circle img-sm"></td>
-            <td>${dataUser.name}</td>
-            <td>${dataUser.email}</td>
-            <td>${(dataUser.admin) ? 'yes' : 'no'}</td>
-            <td>${Utils.dateFormat(dataUser.register)}</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        
-    `;
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
 
-        this.tableEl.appendChild(tr); 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
+
+        this.tableEl.appendChild(tr);
+
         this.updateCount();
+
+    }
+
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
 
     }
     
