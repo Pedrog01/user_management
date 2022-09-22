2e01883f080f40479def239c808ff8c7738e8f02
Pedro Gabriel <pedro7109.j@gmail.com>
2022-09-22 09:16:16 -0300

    I used the command required,to say that it is mandatory to fill in the forms

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,27 +80,39 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name','email','password'].indexOf(field.name) > -1 && !field.value ){
+            
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if (field.name === "gender") {
     
                 if (field.checked) {
-                    user[field.name] = field.value
+                    user[field.name] = field.value;
                 }
     
             }else if(field.name == 'admin'){
 
-                user[field.name] = field.checked
+                user[field.name] = field.checked;
 
 
             } else {
     
-                user[field.name] = field.value
+                user[field.name] = field.value;
     
             }
-    
+            
         });
+
+        if(!isValid){
+            return false;
+        }
     
         return new User(
             user.name, 
