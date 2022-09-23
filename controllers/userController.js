304725239bf700c7334340fe0a6267a62815b6c0
Pedro Gabriel <pedro7109.j@gmail.com>
2022-09-23 09:44:09 -0300

    Deleting Object Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -215,7 +215,7 @@ onEdit(){
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
@@ -229,6 +229,18 @@ onEdit(){
     }
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+            if(confirm ("deseja realmente exluir?")){
+
+                tr.remove();
+                this.updateCount();
+
+            }
+
+        });
+
 
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
