c25c5dcee54516f3f2e63befa2e4dfb512958f49
Pedro Gabriel <pedro7109.j@gmail.com>
2022-09-23 10:24:10 -0300

    storing data in Session with sessionStorage

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
-
+        this.selectAll();
     }
 onEdit(){
 
@@ -96,6 +96,8 @@ onEdit(){
                 (content) => {
 
                     values.photo = content;
+
+                    this.insert(values);
 
                     this.addLine(values);
 
@@ -199,11 +201,51 @@ onEdit(){
         );
 
     }
+
+
+    getUsersStorage(){
+
+
+        let users = [];
+
+        if(sessionStorage.getItem("users")){
+
+            users = JSON.parse(sessionStorage.getItem("users"));
+        }
+            return users;
+
+    }
+
+    selectAll(){
+
+        let users = this.getUsersStorage()
+
+        users.forEach(dataUser =>{
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+
+        });
+
+    }
+
+    insert(data){
+
+        let users = this.getUsersStorage()
+
+        users.push(data);
+
+        sessionStorage.setItem("users",JSON.stringify(users));
+    }
+
     
     addLine(dataUser) {
 
         let tr = document.createElement('tr');
-
+              
         tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
